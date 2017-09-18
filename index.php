434b319685557b873c04810f84e2c383298b4e92
Chris Sciavolino <cds253@cornell.edu>
2017-09-17 20:11:13 -0400

    Initial commit with some really basic structure. No animations or updated content yet.

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Chris Sciavolino</title>
+        
+        <!-- Font Imports -->
+        <link href="https://fonts.googleapis.com/css?family=Lato:300" rel="stylesheet">
+        
+        <!-- Bootstrap CSS -->
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="styles/all.css" type="text/css">
+        
+        <!-- jQuery CDN -->
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+        
+        <!-- Bootstrap JS -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <!--------------------- Banner Section -------------------------->
+        <section id='banner-wrapper'>
+            <div id="banner-background"></div>
+            <div id="banner-title-wrapper">
+                <h1 id="banner-title">Welcome.</h1>
+                <h3 id="banner-subtitle">Subtitle</h3>
+            </div>
+        </section> <!-- end banner-wrapper -->
+        
+        <div class="container">
+            <!--------------------- Overview Section ------------------------>
+            <section id="overview-wrapper">
+                <div class="section-title-wrapper">
+                    <h1 class="section-title">Overview</h1>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-5">
+                        <div class="row">
+                            <div id='overview-collage-wrapper'>
+                                <img src="images/overview/sciavolino.jpg" id="overview-image-1" class="col-md-3 overview-image" alt="Chris Headshot">
+                                <img src="images/overview/group_pic.jpg" id="overview-image-2" class="col-md-offset-2 col-md-3 overview-image" alt="Chris Headshot">
+                                <img src="images/overview/prof_pic.jpg" id="overview-image-3" class="col-md-3 overview-image" alt="Chris Headshot">
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div id='overview-text-wrapper' class='col-md-offset-1 col-md-6'>
+<!--
+                        <div class='subsection-title-wrapper right-align'>
+                            <h1 class='subsection-title'>Chris Sciavolino</h1>
+                        </div>
+-->
+                        <p class='text-block'>
+                            I am Chris Sciavolino, a sophomore Computer Science major at Cornell University. I've been programming since my sophomore year of high school, loving it ever since. Although I've only taken formal classes in Java, Python, and OCaml, I also know Swift through a supplementary course and practice on my own time.
+                        </p>
+                    </div>
+                </div> <!-- end overview row -->
+            </section> <!-- end overview-wrapper -->
+        </div>
+        
+        <footer>
+        
+        </footer>
+    </body>
+</html>
