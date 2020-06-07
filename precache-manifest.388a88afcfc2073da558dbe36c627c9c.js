cc525616a056ab73fbe494e711bd22d2d7b8884c
Chris Sciavolino <cds253@cornell.edu>
2020-06-07 19:30:01 -0400

    Updates

---
+++ precache-manifest.388a88afcfc2073da558dbe36c627c9c.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3d7f646013bb4728067519a33dd2c8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8625fa9886bb10cf7b57",
+    "url": "/static/css/2.81187608.chunk.css"
+  },
+  {
+    "revision": "c28eaf64ea55488c562b",
+    "url": "/static/css/main.fc4fe54c.chunk.css"
+  },
+  {
+    "revision": "8625fa9886bb10cf7b57",
+    "url": "/static/js/2.052d725c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.052d725c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c28eaf64ea55488c562b",
+    "url": "/static/js/main.190206b3.chunk.js"
+  },
+  {
+    "revision": "74238a5a256b2bbf3ad0",
+    "url": "/static/js/runtime-main.989c5c93.js"
+  },
+  {
+    "revision": "78da56b4061114d66b2c489bdd6799d8",
+    "url": "/static/media/ChrisSciavolinoResume.78da56b4.pdf"
+  },
+  {
+    "revision": "38be557f5db705990e67878fc92fd277",
+    "url": "/static/media/RL_Training_Survey.38be557f.pdf"
+  },
+  {
+    "revision": "2a1a6c94fd154fff6afe235cf73ecbba",
+    "url": "/static/media/airbnb-logo-wide.2a1a6c94.svg"
+  },
+  {
+    "revision": "71b61b7f9006a2beec93683a6b611de9",
+    "url": "/static/media/capitalone-logo.71b61b7f.svg"
+  },
+  {
+    "revision": "b249f223da486042c1f72eba103a7255",
+    "url": "/static/media/classifier.b249f223.jpg"
+  },
+  {
+    "revision": "aaae88a37e30831bfbdcba3d16711b6e",
+    "url": "/static/media/cornell-logo.aaae88a3.svg"
+  },
+  {
+    "revision": "ad627b853d804af1130eb8fc6949d462",
+    "url": "/static/media/cornell-sun-app-icon.ad627b85.png"
+  },
+  {
+    "revision": "7191d6cdab9dcd9f687bf2cd805af2ab",
+    "url": "/static/media/drsciavolinoday.7191d6cd.jpg"
+  },
+  {
+    "revision": "9ae28a78961cddb4cd51f9b21122d211",
+    "url": "/static/media/email.9ae28a78.svg"
+  },
+  {
+    "revision": "3ae13bc863b5aebd90257119aa4b22f0",
+    "url": "/static/media/facebook-logo.3ae13bc8.svg"
+  },
+  {
+    "revision": "4f47064557639675c1c1e84b96b0aab3",
+    "url": "/static/media/github.4f470645.svg"
+  },
+  {
+    "revision": "e1613418cea60afab186ac7e5ec2264b",
+    "url": "/static/media/linkedin.e1613418.svg"
+  },
+  {
+    "revision": "1210c96293bb8ec5ae79158beed732bd",
+    "url": "/static/media/princeton-logo.1210c962.svg"
+  },
+  {
+    "revision": "b8e49f68902b445e599b5784c8828cc6",
+    "url": "/static/media/prof_pic.b8e49f68.jpg"
+  },
+  {
+    "revision": "916c58f180e3dedfac3d49e74b48ed2e",
+    "url": "/static/media/redroute-icon.916c58f1.png"
+  },
+  {
+    "revision": "b54f2ecb8184e51fbb0af51ca464cc8b",
+    "url": "/static/media/resume.b54f2ecb.svg"
+  },
+  {
+    "revision": "e2b7f41ad3c56110d8bca61bf52c8303",
+    "url": "/static/media/rl-survey-paper.e2b7f41a.jpg"
+  },
+  {
+    "revision": "006e0031a5e2681c796b0e779fbc3877",
+    "url": "/static/media/sciavotech.006e0031.jpg"
+  },
+  {
+    "revision": "7ced8e5055faa495c658862d4ef44bbe",
+    "url": "/static/media/spoofify.7ced8e50.png"
+  },
+  {
+    "revision": "98f8223c4c47ddd4b1c411417e2eebea",
+    "url": "/static/media/stratego-gameplay.98f8223c.jpg"
+  },
+  {
+    "revision": "9db620335f97c2b2f78da5e43fcd2656",
+    "url": "/static/media/yelp-logo.9db62033.svg"
+  }
+]);
